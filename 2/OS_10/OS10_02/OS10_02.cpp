650cf90d68e865b346e4efc70ae21b3eafa83f84
Kate <2003katy2003@mail.ru>
2023-04-15 19:19:00 +0300

    ,

---
+++ 2/OS_10/OS10_02/OS10_02.cpp
@@ -0,0 +1,103 @@
+﻿#include "HT.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#pragma warning (disable : 4996)
+
+void PrintError(char* str);
+
+int main()
+{
+
+	HT::HTHANDLE* HT = NULL;
+
+	try {
+		std::ifstream inputFile("./params.txt");
+		int capacity, snapshotIntervalSec, maxKeyLength, maxPayloadLength;
+		char fileName[256];
+
+		if (inputFile.is_open()) {
+			inputFile >> capacity >> snapshotIntervalSec >> maxKeyLength >> maxPayloadLength >> fileName;
+			inputFile.close();
+		}
+		else {
+			return 0;
+		}
+
+		HT = HT::Create(capacity, snapshotIntervalSec, maxKeyLength, maxPayloadLength, fileName);
+		if (HT) std::cout << "Create: success\n";
+		else throw "Create: error";
+
+		HT::Element* el1 = new HT::Element("s1", 3, "p1", 3);
+		HT::Element* el2 = new HT::Element("st2", 4, "pa2", 4);
+		HT::Element* el3 = new HT::Element("str3", 5, "pay3", 5);
+
+		if (!HT::Insert(HT, el1) || !HT::Insert(HT, el2) || !HT::Insert(HT, el3)) {
+			PrintError(HT::GetLastError(HT));
+		}
+
+		HT::Element* el;
+		std::cout << "\n-------INSERT------\n" << std::endl;
+		if ((el = HT::Get(HT, el1)) == NULL) PrintError(HT::GetLastError(HT));
+		else HT::Print(el);
+		if ((el = HT::Get(HT, el2)) == NULL) PrintError(HT::GetLastError(HT));
+		else HT::Print(el);
+		if ((el = HT::Get(HT, el3)) == NULL) PrintError(HT::GetLastError(HT));
+		else HT::Print(el);
+
+
+		if (!HT::Update(HT, el1, el3->payload, el3->payloadlength)) {
+			PrintError(HT::GetLastError(HT));
+		}
+		if (!HT::Update(HT, el2, el3->payload, el3->payloadlength)) {
+			PrintError(HT::GetLastError(HT));
+		}
+
+		if (!HT::Snap(HT))
+			PrintError(HT::GetLastError(HT));
+
+		std::cout << "\n-------UPDATE-------\n" << std::endl;
+		if ((el = HT::Get(HT, el1)) == NULL) PrintError(HT::GetLastError(HT));
+		else HT::Print(el);
+		if ((el = HT::Get(HT, el2)) == NULL) PrintError(HT::GetLastError(HT));
+		else HT::Print(el);
+		if ((el = HT::Get(HT, el3)) == NULL) PrintError(HT::GetLastError(HT));
+		else HT::Print(el);
+
+		if (!HT::Delete(HT, el2)) {
+			PrintError(HT::GetLastError(HT));
+		}
+		if (!HT::Delete(HT, el3)) {
+			PrintError(HT::GetLastError(HT));
+		}
+
+		std::cout << "\n-------DELETE-------\n" << std::endl;
+		if ((el = HT::Get(HT, el1)) == NULL) PrintError(HT::GetLastError(HT));
+		else HT::Print(el);
+		if ((el = HT::Get(HT, el3)) == NULL) PrintError(HT::GetLastError(HT));
+		else HT::Print(el);
+
+		std::cout << "\n-------CLOSE-------\n" << std::endl;
+		HT::Close(HT);
+		std::cout << "\n-------OPEN-------\n" << std::endl;
+
+		HT::HTHANDLE* HT2 = HT::Open(fileName);
+		std::cout << "\n-------GET-------\n" << std::endl;
+		if ((el = HT::Get(HT2, el1)) == NULL) PrintError(HT::GetLastError(HT));
+		else HT::Print(el);
+
+		std::cout << "\n-------CLOSE-------\n" << std::endl;
+		HT::Close(HT2);
+	}
+	catch (const char* msg)
+	{
+		std::cout << msg << "\n";
+		if (HT != NULL) std::cout << HT::GetLastError(HT);
+	}
+
+	system("pause");
+	return 0;
+}
+void PrintError(char* str) {
+	std::cout << "error: \t" << str << std::endl;
+}
---
+++ 2/OS_10/OS10_03/OS10_03.cpp
@@ -0,0 +1,76 @@
+﻿#include "HT.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#pragma warning (disable : 4996)
+
+void PrintError(char* str);
+
+int main()
+{
+	HT::HTHANDLE* HT = NULL;
+	HT::HTHANDLE* HT2 = NULL;
+
+	try {
+		std::ifstream inputFile("./params.txt");
+		std::ifstream inputFile2("./paramss.txt");
+		int capacity, snapshotIntervalSec, maxKeyLength, maxPayloadLength;
+		char fileName[256], fileName2[256];
+
+		if (inputFile.is_open()) {
+			inputFile >> capacity >> snapshotIntervalSec >> maxKeyLength >> maxPayloadLength >> fileName;
+			inputFile.close();
+		}
+		else {return 0;}
+
+		if (inputFile2.is_open()) {
+			inputFile2 >> capacity >> snapshotIntervalSec >> maxKeyLength >> maxPayloadLength >> fileName2;
+			inputFile2.close();
+		}
+		else {return 0;}
+		
+		HT = HT::Create(capacity, snapshotIntervalSec, maxKeyLength, maxPayloadLength, fileName);
+		HT2 = HT::Create(capacity, snapshotIntervalSec, maxKeyLength, maxPayloadLength, fileName2);
+
+		HT::Close(HT);
+		HT::Close(HT2);
+
+		std::cout << "-------OPEN 1-------" << std::endl;
+		HT::HTHANDLE* HT1 = HT::Open(fileName);
+		std::cout << "-------OPEN 2-------" << std::endl;
+		HT::HTHANDLE* HT3 = HT::Open(fileName2);
+
+		HT::Element* el1 = new HT::Element("s1", 3, "p1", 3);
+		HT::Element* el2 = new HT::Element("st2", 4, "pa2", 4);
+
+		if (!HT::Insert(HT, el1) || !HT::Insert(HT, el2) || !HT::Insert(HT2, el1) || !HT::Insert(HT2, el2)) {
+			PrintError(HT::GetLastError(HT));
+			PrintError(HT::GetLastError(HT2));
+		}
+
+		HT::Element* el;
+
+		std::cout << "-------GET 1-------" << std::endl;
+		if ((el = HT::Get(HT1, el1)) == NULL) PrintError(HT::GetLastError(HT1));
+		else HT::Print(el);
+		std::cout << "-------GET 2-------" << std::endl;
+		if ((el = HT::Get(HT3, el2)) == NULL) PrintError(HT::GetLastError(HT3));
+		else HT::Print(el);
+
+		std::cout << "-------CLOSE 1-------" << std::endl;
+		HT::Close(HT1);
+		std::cout << "-------CLOSE 2-------" << std::endl;
+		HT::Close(HT3);
+	}
+	catch (const char* msg)
+	{
+		std::cout << msg << "\n";
+		if (HT != NULL) std::cout << HT::GetLastError(HT);
+	}
+
+	system("pause");
+	return 0;
+}
+void PrintError(char* str) {
+	std::cout << "error: \t" << str << std::endl;
+}
